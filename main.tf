6bdd75c9548c3330f1963aa66c826b49c7097c24
SuryaPatra <40135093+SuryaPatra@users.noreply.github.com>
2020-07-23 19:21:41 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+    ami           = "ami-086b16d6badeb5716"
+    instance_type = "t2.micro"
+    key_name = "workstation"
+}
+
+
+output "instance_key" {
+    value = "${aws_instance.example.key_name}"
+}
